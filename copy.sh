4e64dc0ca095822f10278d9722dda5b8bd011ef2
arijustech <arijus.ka@outlook.com>
2025-04-18 09:48:52 +0300

    update

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo cp -r ~/rofi ~/.config/rofi
-sudo rm -r ~/.config/rofi/.git
-sudo chmod 644 ~/.config/rofi/config.rasi
-sudo chmod 774 ~/.config/rofi/powermenu.sh
-sudo chmod 644 ~/.config/rofi/themes/nord.rasi
-sudo chmod 644 ~/.config/rofi/themes/powermenu.rasi
-sudo chmod 644 ~/.config/rofi/themes/sidetab-nord.rasi
